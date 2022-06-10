c4b86458c9f3e2d31fc808c049d18f76db077ad4
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-06-10 21:55:45 +1200

    complete

--- script.js
+++ script.js
@@ -4,25 +4,33 @@ let circleTurn = false;
 
 const cellEl =  document.querySelectorAll('[data-cell');
 const board =  document.querySelector('#board');
+const winnerText =  document.querySelector('h1');
 
-
-cellEl.forEach((cell) => {
-   return cell.addEventListener('click', handleClick, { once: true })
-})
 
 const winningCombinations = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [1, 2, 3],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
 
 function handleClick(e) {
     const cell = e.target;
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-
     placeMark(cell, currentClass);
+    if (checkWin(currentClass)){
+       return endGame(currentClass)
+    }
+
     switchTurns()
+    setBoardHoverClass()
 
     //check for win
     //check for draw
@@ -33,16 +41,42 @@ function placeMark(cell, currentClass){
     
 }
 
-function switchTurns(){
-    circleTurn = !circleTurn;
+function setBoardHoverClass() {
     board.classList.remove(circleTurn ? X_CLASS : CIRCLE_CLASS)
     board.classList.add(circleTurn ? CIRCLE_CLASS : X_CLASS)
 }
 
+function switchTurns(){
+    circleTurn = !circleTurn;
+}
+
 function intGame() {
+    cellEl.forEach((cell) => {
+        return cell.addEventListener('click', handleClick, { once: true })
+     })
+
     const pickStarter = Math.floor(Math.random() * (2 - 1 + 1 ) + 1);
     pickStarter === 1 ? circleTurn = true : circleTurn = false;
-    board.classList.add(circleTurn ? X_CLASS : CIRCLE_CLASS)
+    setBoardHoverClass()
+}
+
+function checkWin(currentClass){
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cellEl[index].classList.contains(currentClass)
+        })
+    })
+}
+
+function endGame(winner) {
+    winnerText.style.display = 'block';
+    winnerText.textContent = `${winner} wins`;
+    board.classList.remove(CIRCLE_CLASS);
+    board.classList.remove(X_CLASS);
+    
+    cellEl.forEach((cell) => {
+        return cell.removeEventListener('click', handleClick)
+     })
 }
 
 intGame();
