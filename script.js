583bbbbe578ab0fc7f9b736ac0f994c6a4e34b32
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-06-10 21:25:22 +1200

    new game who dis

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle'
+let circleTurn = false;
 
-const Gameboard =  (() => {
-    let gameBoard = new Array(9);
-    return gameBoard;
-})();
+const cellEl =  document.querySelectorAll('[data-cell');
+const board =  document.querySelector('#board');
 
 
-Gameboard.gameBoard();
+cellEl.forEach((cell) => {
+   return cell.addEventListener('click', handleClick, { once: true })
+})
+
+const winningCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [1, 2, 3],
+]
+
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+
+    placeMark(cell, currentClass);
+    switchTurns()
+
+    //check for win
+    //check for draw
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass);
+    
+}
+
+function switchTurns(){
+    circleTurn = !circleTurn;
+    board.classList.remove(circleTurn ? X_CLASS : CIRCLE_CLASS)
+    board.classList.add(circleTurn ? CIRCLE_CLASS : X_CLASS)
+}
+
+function intGame() {
+    const pickStarter = Math.floor(Math.random() * (2 - 1 + 1 ) + 1);
+    pickStarter === 1 ? circleTurn = true : circleTurn = false;
+    board.classList.add(circleTurn ? X_CLASS : CIRCLE_CLASS)
+}
+
+intGame();
